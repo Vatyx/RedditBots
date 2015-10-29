d61a895a429935ce9fe9885b4d060841b56bb3f7
Sahil Dhanju <sahildhanju1@gmail.com>
2015-10-28 23:45:27 -0500

    Found a package!

--- InstagramUploader/instagramtest.py
+++ InstagramUploader/instagramtest.py
@@ -1,7 +1,8 @@
-from urllib2 import Request, urlopen, URLError
+from instagram.client import InstagramAPI
 
-request = Request('http://placekitten.com/')
-
-try:
-	response = urlopen(request)
-	kittens = response.read()
+api = InstagramAPI(client_id='ad894ecb14cb4928bd00f5e4d8c2ae67', client_secret='b5deae6b21be45259ccdd272998a2651')
+popular_media = api.media_popular(count=20)
+f = open("links", 'w')
+for media in popular_media:
+	f.write(media.images['standard_resolution'].url + "\n")
+    # print(media.images['standard_resolution'].url)
